10f0173690a58e75f7fc7dc0ac517b17d124e83a
HPecene <34321539+HPecene@users.noreply.github.com>
2020-01-27 15:09:16 -1200

    Create ImprimirLn.cs
    Se crea la clase que maneja el comando de Imprimir con Salto

---
+++ InterpreteSencillo/InterpreteSencillo/arbol/ImprimirLn.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterpreteSencillo.arbol
+{
+    class ImprimirLn : Instruccion
+    {
+        /**
+     * Contenido que será impreso al ejecutar la instrucción imprimir, este debe
+     * ser una instrucción que genere un valor al ser ejecutada.
+     */
+        private Instruccion contenido;
+    /**
+     * Constructor de la clase imprimir
+     * @param contenido contenido que será impreso al ejecutar la instrucción
+     */
+    public ImprimirLn(Instruccion contenido)
+        {
+            this.contenido = contenido;
+        }
+        /**
+         * Método que ejecuta la accion de imprimir un valor, es una sobreescritura del 
+         * método ejecutar que se debe programar por la implementación de la interfaz
+         * instrucción
+         * @param ts Tabla de símbolos del ámbito padre de la sentencia.
+         * @return Esta instrucción retorna nulo porque no produce ningun valor al ser
+         * ejecutada.
+         */
+        
+    public Object ejecutar(TablaDeSimbolos ts)
+        {
+            String impresion = contenido.ejecutar(ts).ToString();
+            System.Diagnostics.Debug.Write(impresion);
+            return null;
+        }
+    }
+}
